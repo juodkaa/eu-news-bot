159ec6ce6a493de48fa7f966c55044fda8b8d984
juodkaa <juodka_a@yahoo.com>
2025-06-16 12:23:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+from flask import Flask
 import requests
 from bs4 import BeautifulSoup
 
+app = Flask(__name__)
+
+@app.route('/')
 def fetch_news():
     url = "https://ec.europa.eu/commission/presscorner/home/en"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
 
     headlines = soup.find_all("a", class_="press-release-title")[:5]
+    output = []
+
     for item in headlines:
-        print(item.text.strip(), "|", "https://ec.europa.eu" + item["href"])
+        title = item.text.strip()
+        link = "https://ec.europa.eu" + item["href"]
+        output.append(f"{title} | <a href='{link}'>{link}</a>")
+
+    return "<br><br>".join(output)
 
 if __name__ == "__main__":
-    fetch_news()
+    app.run(host="0.0.0.0", port=10000)
